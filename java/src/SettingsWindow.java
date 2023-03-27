1a9e3db9ca1929bd0a77353652a914ca056c8592
Daniel Delgado Jiménez <danieldelgadojimenez@protonmail.com>
2023-03-27 23:18:29 +0200

    test

--- java/src/SettingsWindow.java
+++ java/src/SettingsWindow.java
@@ -1,2 +1,13 @@
-public class SettingsWindow {
+import javax.swing.*;
+
+public class SettingsWindow extends JFrame {
+    private JPanel hjuhgghj;
+    private JScrollBar scrollBar1;
+    private JPasswordField passwordField1;
+    private JCheckBox checkBox1;
+
+    public static void main(String[] args) {
+        SettingsWindow settingsWindow = new SettingsWindow();
+        settingsWindow.setVisible(true);
+    }
 }
