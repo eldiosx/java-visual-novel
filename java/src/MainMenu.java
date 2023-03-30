c77d93c6ff5e1cec6a346d216d003275a6eac6dc
Daniel Delgado Jiménez <danieldelgadojimenez@protonmail.com>
2023-03-30 10:05:59 +0200

    javaFXfix

--- java/src/MainMenu.java
+++ java/src/MainMenu.java
@@ -1,90 +1,95 @@
-import java.awt.*;
-import java.awt.image.BufferedImage;
-import java.awt.event.*;
-import javax.swing.*;
-import javax.swing.border.*;
-import java.io.IOException;
-import java.io.File;
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Dimension2D;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 import javax.sound.sampled.*;
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.Clip;
+import java.io.File;
+import java.io.IOException;
 //import javax.imageio.ImageIO;
 
-public class MainMenu extends JFrame {
-    private static final long serialVersionUID = 1L;
-	private Clip bgMusic;
-    private ImageIcon backgroundImage;
-    private JLabel titleLabel;
-    private JButton startButton;
-    private ImageIcon startImage;
-    private JButton settingsButton;
-    private ImageIcon settingsImage;
-    private JButton exitButton;
-    private ImageIcon exitImage;
+public class MainMenu extends Application {
+    private Clip bgMusic;
+    private Image backgroundImage;
+    private Button startButton;
+    private Image startImage;
+    private Button settingsButton;
+    private Image settingsImage;
+    private Button exitButton;
+    private Image exitImage;
 
-    public MainMenu() {
-        // Configurar la ventana
-        setTitle("Tu videojuego favorito de serie B");
-        setMinimumSize(new Dimension(800, 600));
-        setExtendedState(Frame.MAXIMIZED_BOTH); // Maximiza la ventana al tamaño de la pantalla
-        // setSize(800, 600); // Tamaño inicial
-        setResizable(true); // Permitir redimensionamiento
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-
-        // Configurar el diseño del contenido
-        JPanel contentPanel = new JPanel(new BorderLayout()) {
-            private static final long serialVersionUID = 1L;
-
-			@Override
-            public void paintComponent(Graphics g) {
-                super.paintComponent(g);
-                g.drawImage(backgroundImage.getImage(), 0, 0, getWidth(), getHeight(), null);
+    @Override
+    public void start(Stage primaryStage) {
+        BorderPane root = new BorderPane();
+        root.setPrefSize(800, 600);
+        primaryStage.setResizable(true);
+        primaryStage.setTitle("Tu videojuego favorito de serie B");
+        primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+            @Override
+            public void handle(WindowEvent event) {
+                Platform.exit();
+                System.exit(0);
             }
-        };
-        contentPanel.setBackground(Color.BLACK); // Establecer el color de fondo
-        setContentPane(contentPanel);
-        // Titulo, me falta la sombrita o cambiar por una imagen
-        titleLabel = new JLabel("Tu videojuego favorito de serie B");
-        titleLabel.setBorder(new EmptyBorder(10, 0, 10, 0));
-        titleLabel.setForeground(Color.gray);
-        titleLabel.setFont(new Font("Sans Serif", Font.BOLD, 48));
-        titleLabel.setHorizontalAlignment(JLabel.CENTER);
-        contentPanel.add(titleLabel, BorderLayout.NORTH);
+        });
 
-        // Configurar los botones
+        // Configurar el contenido
+        StackPane contentPane = new StackPane();
+        contentPane.setStyle("-fx-background-color: black;");
+        root.setCenter(contentPane);
+
+        // Titulo
+        Text titleLabel = new Text("Tu videojuego favorito de serie B");
+        titleLabel.setFont(Font.font("Sans Serif", FontWeight.BOLD, 48));
+        titleLabel.setStyle("-fx-text-fill: gray;");
+        contentPane.getChildren().add(titleLabel);
+
+        // Botones
         // Start
-        startImage = new ImageIcon("../assets/icons/start.png");
-        startButton = new JButton();
-        startButton.setIcon(startImage);
-        startButton.setOpaque(false); // Eliminar fondo
-        startButton.setContentAreaFilled(false); // Eliminar fondo
-        startButton.setBorderPainted(false); // Eliminar borde
-        // Setting
-        settingsImage = new ImageIcon("../assets/icons/setting.png");
-        settingsButton = new JButton();
-        settingsButton.setIcon(settingsImage);
-        settingsButton.setOpaque(false);
-        settingsButton.setContentAreaFilled(false);
-        settingsButton.setBorderPainted(false);
+        startImage = new Image(new File("../assets/icons/start.png").toURI().toString());
+        startButton = new Button();
+        startButton.setGraphic(new ImageView(startImage));
+        startButton.setPadding(new Insets(0));
+        // Settings
+        settingsImage = new Image(new File("../assets/icons/setting.png").toURI().toString());
+        settingsButton = new Button();
+        settingsButton.setGraphic(new ImageView(settingsImage));
+        settingsButton.setPadding(new Insets(0));
         // Exit
-        exitImage = new ImageIcon("../assets/icons/exit.png");
-        exitButton = new JButton();
-        exitButton.setIcon(exitImage);
-        exitButton.setOpaque(false);
-        exitButton.setContentAreaFilled(false);
-        exitButton.setBorderPainted(false);
-        // Panel
-        JPanel buttonPanel = new JPanel(new GridLayout(3, 1, 0, 0));
-        buttonPanel.setBorder(new EmptyBorder(200, 200, 60, 200));
-        buttonPanel.setOpaque(false); // Establecer como no opaco para que se muestre el fondo de la ventana
-        buttonPanel.add(startButton);
-        buttonPanel.add(settingsButton);
-        buttonPanel.add(exitButton);
-        contentPanel.add(buttonPanel, BorderLayout.CENTER);
+        exitImage = new Image(new File("../assets/icons/exit.png").toURI().toString());
+        exitButton = new Button();
+        exitButton.setGraphic(new ImageView(exitImage));
+        exitButton.setPadding(new Insets(0));
 
-        // Configurar el fondo de pantalla
-        backgroundImage = new ImageIcon("../assets/images/background.jpg");
+        GridPane buttonGrid = new GridPane();
+        buttonGrid.setVgap(40);
+        buttonGrid.setAlignment(Pos.CENTER);
+        buttonGrid.add(startButton, 0, 0);
+        buttonGrid.add(settingsButton, 0, 1);
+        buttonGrid.add(exitButton, 0, 2);
+        contentPane.getChildren().add(buttonGrid);
 
+        // Configurar la imagen de fondo
+        backgroundImage = new Image(new File("../assets/images/background.jpg").toURI().toString());
+        ImageView backgroundImageView = new ImageView(backgroundImage);
+        backgroundImageView.fitWidthProperty().bind(primaryStage.widthProperty());
+        backgroundImageView.fitHeightProperty().bind(primaryStage.heightProperty());
+        contentPane.getChildren().add(backgroundImageView);
+        
         // SONIDO (En un futuro creare una clase aparte con todos los sonidos aparte)
         // Cargar la música de fondo (Cambiar el soporte para .ogg)
         try {
@@ -144,15 +149,20 @@ public class MainMenu extends JFrame {
     private ImageIcon scaleImage(Image image, int width, int height) {
         BufferedImage resizedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
         Graphics2D g2d = resizedImage.createGraphics();
+        g2d.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
         g2d.drawImage(image, 0, 0, width, height, null);
         g2d.dispose();
         return new ImageIcon(resizedImage);
     }
 
     public static void main(String[] args) {
-        MainMenu mainMenu = new MainMenu();
-        mainMenu.setVisible(true);
-        
+        // Iniciar la ventana principal
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                MainMenu mainMenu = new MainMenu();
+                mainMenu.setVisible(true);
+            }
+        });
     }
-    
 }
+        
