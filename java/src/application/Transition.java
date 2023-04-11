268aa2c6ba985b6ad9ddda5952d347828abe74e6
AdrianMarque <nadiraryuu22@gmail.com>
2023-04-11 14:42:58 +0200

    Cambios de la base de transicion y el añadido de las imagenes
    Imagenes proporcionadas por Aida Fernandez

--- java/src/application/Transition.java
+++ java/src/application/Transition.java
@@ -1,6 +1,9 @@
 package application;
 
+import java.io.File;
+
 import javafx.application.Application;
+import javafx.scene.control.Button;
 import javafx.beans.binding.DoubleBinding;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
@@ -11,79 +14,102 @@ import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
-import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 public class Transition extends Application {
-	private static final String RESOURCES_PATH = "assets";
-	private static final String BACKGROUND_URL = "file:" + RESOURCES_PATH + "/images/horror.gif";
-	private static final double LABEL_WIDTH = 200.0; // Ancho del cuadro de texto
-	private static final double MARGIN_LEFT = 50.0; // Margen izquierdo
-	private static final double MARGIN_BOTTOM = 50.0; // Margen inferior
+    private Font titleFont;
+    private static final String RESOURCES_PATH = new File("assets").getAbsolutePath();
+    private static final String BACKGROUND_URL = "file:" + RESOURCES_PATH + "/images/horror.gif";
+    private static final double MARGIN_LEFT = 50.0;
+    private static final double MARGIN_BOTTOM = 50.0;
 
-	@Override
-	public void start(Stage primaryStage) {
-		// Configuración de la ventana principal
-		primaryStage.setTitle("Mi Aplicación"); // Título de la ventana
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Episodio 1");
+        // Configurar la ventana
+        primaryStage.setTitle("Tu videojuego favorito de serie B"); // Título de la ventana
+        primaryStage.setResizable(true); // Se puede redimensionar
+        primaryStage.setFullScreen(true); // Abre la ventana en pantalla completa
+        primaryStage.setMinWidth(800); // Establece el ancho mínimo de la ventana en 800px
+        primaryStage.setMinHeight(600); // Establece la altura mínima de la ventana en 600px
+        primaryStage.show();
+        
+        BorderPane borderPane = new BorderPane();
+        //imagen de fondo a la escena
+        String ImagenFondo = (RESOURCES_PATH + "/images/fondo1.jpg");
+        addBackgroundImageToScene(borderPane, ImagenFondo);
+        //imagen de la persona
+        String ImagenPersona = (RESOURCES_PATH + "");//para la imagen de la persona
+        
+        
+        
+        //caja de texto
+        VBox vBox = new VBox(); // Crea un VBox para organizar los nodos verticalmente
+        vBox.setAlignment(Pos.BOTTOM_LEFT); // Alinea los nodos al centro del VBox
+        vBox.setSpacing(10); // Establece un espacio de 10 píxeles entre los nodos en el VBox
+        //label de texto
+        Label labelBienvenida = new Label(
+                "Bienvenido al campamento \"The Otter\". Espero que estés preparado para dos semanas de diversión. Tus compañeros ya están ubicados en sus cabañas. La tuya es la cabaña número 7, puedes ir a acomodarte si quieres .");
+        labelBienvenida.setWrapText(true);
+        labelBienvenida.setFont(new Font(12));
+        BackgroundFill backgroundFill = new BackgroundFill(Color.rgb(0, 0, 139, 0.5), new CornerRadii(10), null);
+        labelBienvenida.setBackground(new Background(backgroundFill));
+        labelBienvenida.setTextFill(Color.YELLOW);
+        labelBienvenida.setStyle("-fx-padding: 20px;");
+        //label de opciones
+        Button buttonOpcion1 = new Button ("Ir a la cabaña");
+        buttonOpcion1.setWrapText(true);
+        buttonOpcion1.setFont(new Font(12));
+        BackgroundFill FillOpcion1 = new BackgroundFill(Color.rgb(0, 0, 139, 0.5), new CornerRadii(10), null);
+        buttonOpcion1.setBackground(new Background(FillOpcion1));
+        buttonOpcion1.setTextFill(Color.YELLOW);
+        buttonOpcion1.setStyle("-fx-padding: 20px;");
+        buttonOpcion1.setOnMouseClicked(event -> {
+            SoundBox.playSound(RESOURCES_PATH + "/audio/click.wav");
+            // Agrega aquí el código que deseas ejecutar cuando se haga clic en el botón "Opción1"
+        });
+        
+     // Agrega la Label al VBox
+        vBox.getChildren().add(labelBienvenida); 
+        vBox.getChildren().add(buttonOpcion1); 
 
-		// Panel principal con diseño de borde
-		BorderPane borderPane = new BorderPane();
+        Label otroLabel = new Label(""); // Crea otro Label
+        otroLabel.setFont(new Font(14));
+        vBox.getChildren().add(otroLabel); // Agrega el otro Label al VBox
 
-		// Cuadro de texto estilo mensaje
-		Label label = new Label(
-				"Bienvenido al campamento \"The Otter\". Espero que estés preparado para dos semanas de diversión. Tus compañeros ya están ubicados en sus cabañas. La tuya es la cabaña número 7, puedes ir a acomodarte si quieres ."); // Texto
-																																																											// de
-																																																											// ejemplo
-		label.setWrapText(true); // Activar ajuste de texto
-		label.setMaxWidth(LABEL_WIDTH); // Ancho máximo del cuadro de texto
+        borderPane.setCenter(vBox); // Agrega el VBox al centro del BorderPane
 
-		// Configurar el fondo del Label con un color azul oscuro con opacidad
-		BackgroundFill backgroundFill = new BackgroundFill(Color.rgb(0, 0, 139, 0.5), new CornerRadii(10), null);
-		label.setBackground(new Background(backgroundFill));
+        Image backgroundImage = new Image(BACKGROUND_URL);
+        ImageView backgroundImageView = new ImageView(backgroundImage);
+        borderPane.getChildren().add(0, backgroundImageView);
+        borderPane.setMargin(vBox, new javafx.geometry.Insets(MARGIN_BOTTOM, 0, 0, MARGIN_LEFT));
 
-		// Configurar el color de la fuente a amarillo
-		label.setTextFill(Color.YELLOW);
+        DoubleBinding labelWidthBinding = new DoubleBinding() {
+            {
+                super.bind(primaryStage.widthProperty());
+            }
 
-		// Configurar la fuente de la letra
-		label.setFont(Font.font("Arial", 16)); // Cambiar la fuente y tamaño de la letra
+            @Override
+            protected double computeValue() {
+                return primaryStage.getWidth() * 0.2;
+            }
+        };
+        labelBienvenida.maxWidthProperty().bind(labelWidthBinding);
+        Scene scene = new Scene(borderPane);
+        primaryStage.setScene(scene);
+        primaryStage.show(); // Asegúrate de llamar a show() para mostrar la ventana en pantalla
+    }
 
-		// Panel de cuadro de texto en la parte inferior izquierda
-		BorderPane labelsBox = new BorderPane();
-		labelsBox.setPrefHeight(80); // Altura del cuadro de texto
-		labelsBox.setLeft(label);
-		borderPane.setLeft(labelsBox);
-
-		// Imagen de fondo
-		Image backgroundImage = new Image(BACKGROUND_URL);
-		ImageView backgroundImageView = new ImageView(backgroundImage);
-		borderPane.getChildren().add(0, backgroundImageView);
-
-		// Ajustar el margen del cuadro de texto
-		borderPane.setMargin(labelsBox, new javafx.geometry.Insets(0, 0, MARGIN_BOTTOM, MARGIN_LEFT));
-
-		// Ajustar el ancho del cuadro de texto usando DoubleBinding
-		DoubleBinding labelWidthBinding = new DoubleBinding() {
-			{
-				super.bind(borderPane.widthProperty());
-			}
-
-			@Override
-			protected double computeValue() {
-				return borderPane.getWidth() * 0.2; // para poner el ancho al 20%
-			}
-		};
-
-		label.maxWidthProperty().bind(labelWidthBinding);
-
-		// para crear la ventana
-		Scene scene = new Scene(borderPane);
-		primaryStage.setScene(scene);
-
-	}
-
-	public static void main(String[] args) {
-		launch(args);
-	}
+    public static void main(String[] args) {
+        launch(args);
+    }
+    private void addBackgroundImageToScene(BorderPane pane, String imageUrl) {
+        Image backgroundImage = new Image(imageUrl);
+        ImageView backgroundImageView = new ImageView(backgroundImage);
+        pane.getChildren().add(0, backgroundImageView);
+    }
+    
 }
