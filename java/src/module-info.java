8c6231a05ba3807ea4c50d596880324582fd8867
nicolasscano <ncdecelis@gmail.com>
2023-04-13 13:52:25 +0200

    d3edd

--- java/src/module-info.java
+++ java/src/module-info.java
@@ -1,15 +1,11 @@
 module java {
 	requires javafx.controls;
 	requires javafx.media;
-	requires java.desktop;
 	requires javafx.fxml;
 	requires java.sql;
-<<<<<<< Updated upstream
 	requires mysql.connector.j;
-=======
 //	requires java.desktop;
 //	requires mysql.connector.j;
->>>>>>> Stashed changes
 	
 	opens application to javafx.graphics, javafx.fxml;
 }
