b313a54255e03601f34f4ed9847c497cf07f0204
Daniel Delgado Jiménez <danieldelgadojimenez@protonmail.com>
2023-04-13 14:00:46 +0200

    updateX

--- java/src/module-info.java
+++ java/src/module-info.java
@@ -1,10 +1,11 @@
 module java {
 	requires javafx.controls;
 	requires javafx.media;
-	requires java.desktop;
 	requires javafx.fxml;
 	requires java.sql;
-	requires mysql.connector.j;
+	requires java.desktop;
+//	requires mysql.connector.j;
 	
 	opens application to javafx.graphics, javafx.fxml;
+	exports application.Ep1 to javafx.graphics, javafx.fxml;
 }
