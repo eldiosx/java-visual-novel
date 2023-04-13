c3066f8685dbaf52b1b86d8ae04f019be9ec3e6f
Daniel Delgado Jiménez <danieldelgadojimenez@protonmail.com>
2023-04-13 12:08:18 +0200

    Update module-info.java

--- java/src/module-info.java
+++ java/src/module-info.java
@@ -4,7 +4,6 @@ module java {
 	requires java.desktop;
 	requires javafx.fxml;
 	requires java.sql;
-	requires java.desktop;
 	requires mysql.connector.j;
 	
 	opens application to javafx.graphics, javafx.fxml;
