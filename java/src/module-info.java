937b751222756d74b02f52f381c0c82fa57a420b
Daniel Delgado Jiménez <danieldelgadojimenez@protonmail.com>
2023-04-13 09:25:11 +0200

    minorFix and Test

--- java/src/module-info.java
+++ java/src/module-info.java
@@ -2,6 +2,8 @@ module java {
 	requires javafx.controls;
 	requires javafx.media;
 	requires java.desktop;
+	requires javafx.fxml;
+	requires java.sql;
 	
 	opens application to javafx.graphics, javafx.fxml;
 }
