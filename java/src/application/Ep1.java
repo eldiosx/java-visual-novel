1f64bf32cabcc9842d0314bc5b4fb00320b4d99e
AidaFdz <aidafdz@hotmail.com>
2023-04-13 14:14:10 +0200

    Novedades

---
+++ java/src/application/Ep1.java
@@ -0,0 +1,24 @@
+package application;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Ep1 extends Application {
+	//Point And Click Niña Muerta
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("EP1NinaMuerta.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Episodio 1");
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
