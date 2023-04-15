7465654afdc854f48e07c0c3db3f6f2ed984d9f5
Daniel Delgado Jiménez <danieldelgadojimenez@protonmail.com>
2023-04-15 15:23:55 +0200

    purge and +Help

--- java/src/application/OggPlayer.java
+++ java/src/application/OggPlayer.java
@@ -8,6 +8,20 @@ import javax.sound.sampled.DataLine;
 import javax.sound.sampled.SourceDataLine;
 import com.jcraft.jorbis.*;
 
+
+//Ejemplo para llamar a estas funciones:
+/*
+package application;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        OggPlayer oggPlayer = new OggPlayer();
+        oggPlayer.playAudio("assets/audio/lullabyX.ogg");
+        Thread.sleep(5000); // Esperar 5 segundos
+        oggPlayer.stopAudio();
+    }
+}
+*/
 public class OggPlayer {
     private volatile boolean isPlaying = false;
     private Thread audioThread;
