8855bb070c9c850a9745be448b16ec5805ccf97a
Daniel Delgado Jiménez <danieldelgadojimenez@protonmail.com>
2023-04-19 11:03:20 +0200

    Revert "Revert "Merge branch 'main' of https://github.com/eldiosx/novelaGraficaJava""
    This reverts commit 2c8684c0cd61e594a2869b17bd85f4c224169468.

--- java/src/application/BackgroundMusic.java
+++ java/src/application/BackgroundMusic.java
@@ -2,89 +2,88 @@ package application;
 
 import java.io.FileInputStream;
 import java.io.InputStream;
+
 import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.DataLine;
+import javax.sound.sampled.FloatControl;
 import javax.sound.sampled.SourceDataLine;
 
+import javafx.application.Platform;
 
-//Ejemplo para llamar a estas funciones:
-/*
-package application;
-
-public class Main {
-    public static void main(String[] args) throws InterruptedException {
-        OggPlayer oggPlayer = new OggPlayer();
-        oggPlayer.playAudio("assets/audio/lullabyX.ogg");
-        Thread.sleep(5000); // Esperar 5 segundos
-        oggPlayer.stopAudio();
-    }
-}
-*/
 public class BackgroundMusic {
-    private volatile boolean isPlaying = false;
-    private Thread audioThread;
-    private String filePath;
+    private float volume = 0.5f; // Valor de volumen por defecto
+    private SourceDataLine sourceDataLine;
 
     public void playAudio(String filePath) {
-        if (isPlaying) {
-            System.out.println("Audio already playing.");
-            return;
-        }
-
         this.setFilePath(filePath);
-        isPlaying = true;
-        audioThread = new Thread(() -> {
-            try {
-                // Cargar archivo de audio Ogg
-                InputStream in = new FileInputStream(filePath);
-                try (OggInputStream oggIn = new OggInputStream(in)) {
-					int channels = 2; // número de canales (estéreo)
-					int rate = 44100; // frecuencia de muestreo (44100 Hz)
-					int bufferSize = 4096; // tamaño del búfer de audio
-					// Crear objeto de formato de audio
-					AudioFormat audioFormat = new AudioFormat((float) rate, 16, channels, true, false);
 
-					// Crear objeto de línea de datos de origen
-					DataLine.Info dataLineInfo = new DataLine.Info(SourceDataLine.class, audioFormat);
-					SourceDataLine sourceDataLine = (SourceDataLine) AudioSystem.getLine(dataLineInfo);
+        Thread backgroundThread = new Thread(() -> {
+            try (InputStream in = new FileInputStream(filePath);
+                    OggInputStream oggIn = new OggInputStream(in)) {
 
-					// Abrir línea de datos de origen
-					sourceDataLine.open(audioFormat);
-					sourceDataLine.start();
+                int channels = 2; // número de canales (estéreo)
+                int rate = 44100; // frecuencia de muestreo (44100 Hz)
+                int bufferSize = 4096; // tamaño del búfer de audio
 
-					// Reproducir audio
-					byte[] buffer = new byte[bufferSize];
-					while (isPlaying) {
-					    int bytesRead = oggIn.read(buffer, 0, bufferSize);
-					    if (bytesRead == -1) {
-					        break;
-					    }
-					    sourceDataLine.write(buffer, 0, bytesRead);
-					}
+                // Crear objeto de formato de audio
+                AudioFormat audioFormat = new AudioFormat((float) rate, 16, channels, true, false);
 
-					// Cerrar línea de datos de origen
-					sourceDataLine.drain();
-					sourceDataLine.stop();
-					sourceDataLine.close();
-				}
+                // Crear objeto de línea de datos de origen
+                DataLine.Info dataLineInfo = new DataLine.Info(SourceDataLine.class, audioFormat);
+                sourceDataLine = (SourceDataLine) AudioSystem.getLine(dataLineInfo);
+
+                // Abrir línea de datos de origen
+                sourceDataLine.open(audioFormat);
+                sourceDataLine.start();
+
+                // Configurar el volumen de la línea de datos de origen
+                FloatControl gainControl = (FloatControl) sourceDataLine.getControl(FloatControl.Type.MASTER_GAIN);
+                float dB = (float) (Math.log(volume) / Math.log(10.0) * 20.0);
+                gainControl.setValue(dB);
+
+                // Reproducir audio
+                byte[] buffer = new byte[bufferSize];
+                int bytesRead;
+                while ((bytesRead = oggIn.read(buffer, 0, bufferSize)) != -1) {
+                    if (sourceDataLine != null) {
+                        sourceDataLine.write(buffer, 0, bytesRead);
+                    } else {
+                        break;
+                    }
+                }
+
             } catch (Exception e) {
                 e.printStackTrace();
+            } finally {
+                if (sourceDataLine != null) {
+                    sourceDataLine.drain();
+                    sourceDataLine.stop();
+                    sourceDataLine.close();
+                }
             }
+
         });
-        audioThread.start();
+        backgroundThread.start();
+    }
+
+    private void setFilePath(String filePath) {
+    }
+
+    public void setVolume(float volume) {
+        this.volume = volume;
+        if (sourceDataLine != null) {
+            FloatControl gainControl = (FloatControl) sourceDataLine.getControl(FloatControl.Type.MASTER_GAIN);
+            float dB = (float) (Math.log(volume) / Math.log(10.0) * 20.0);
+            gainControl.setValue(dB);
+        }
     }
 
     public void stopAudio() {
-        isPlaying = false;
-        audioThread = null;
+        if (sourceDataLine != null) {
+            sourceDataLine.stop();
+            sourceDataLine.close();
+            sourceDataLine = null;
+        }
     }
-
-	public String getFilePath() {
-		return filePath;
-	}
-
-	public void setFilePath(String filePath) {
-		this.filePath = filePath;
-	}
 }
--- java/src/application/VoiceBox.java
+++ java/src/application/VoiceBox.java
@@ -1,73 +1,59 @@
 package application;
-
 import java.io.FileInputStream;
 import java.io.InputStream;
+
 import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.DataLine;
+import javax.sound.sampled.FloatControl;
 import javax.sound.sampled.SourceDataLine;
 
-
-//Ejemplo para llamar a estas funciones IMPORTANTE HAY QUE USAR EL stop PARA REPRODUCIR UN NUEVO AUDIO:
-/*
-package application;
-
-public class Main {
-    public static void main(String[] args) throws InterruptedException {
-        VoiceBox voiceBox = new VoiceBox();
-        voiceBox.playAudio("assets/audio/lullabyX.ogg");
-        Thread.sleep(5000); // Esperar 5 segundos
-        voiceBox.stopAudio();
-    }
-}
-*/
 public class VoiceBox {
-    private volatile boolean isPlaying = false;
-    private Thread audioThread;
-    private String filePath;
+    private float volume = 0.5f; // Valor de volumen por defecto
 
     public void playAudio(String filePath) {
-        if (isPlaying) {
-            System.out.println("Audio already playing.");
-            return;
-        }
-
         this.setFilePath(filePath);
-        isPlaying = true;
-        audioThread = new Thread(() -> {
+
+        Thread audioThread = new Thread(() -> {
             try {
                 // Cargar archivo de audio Ogg
                 InputStream in = new FileInputStream(filePath);
                 try (OggInputStream oggIn = new OggInputStream(in)) {
-					int channels = 2; // número de canales (estéreo)
-					int rate = 44100; // frecuencia de muestreo (44100 Hz)
-					int bufferSize = 4096; // tamaño del búfer de audio
-					// Crear objeto de formato de audio
-					AudioFormat audioFormat = new AudioFormat((float) rate, 16, channels, true, false);
+                    int channels = 2; // número de canales (estéreo)
+                    int rate = 44100; // frecuencia de muestreo (44100 Hz)
+                    int bufferSize = 4096; // tamaño del búfer de audio
 
-					// Crear objeto de línea de datos de origen
-					DataLine.Info dataLineInfo = new DataLine.Info(SourceDataLine.class, audioFormat);
-					SourceDataLine sourceDataLine = (SourceDataLine) AudioSystem.getLine(dataLineInfo);
+                    // Crear objeto de formato de audio
+                    AudioFormat audioFormat = new AudioFormat((float) rate, 16, channels, true, false);
 
-					// Abrir línea de datos de origen
-					sourceDataLine.open(audioFormat);
-					sourceDataLine.start();
+                    // Crear objeto de línea de datos de origen
+                    DataLine.Info dataLineInfo = new DataLine.Info(SourceDataLine.class, audioFormat);
+                    SourceDataLine sourceDataLine = (SourceDataLine) AudioSystem.getLine(dataLineInfo);
 
-					// Reproducir audio
-					byte[] buffer = new byte[bufferSize];
-					while (isPlaying) {
-					    int bytesRead = oggIn.read(buffer, 0, bufferSize);
-					    if (bytesRead == -1) {
-					        break;
-					    }
-					    sourceDataLine.write(buffer, 0, bytesRead);
-					}
+                    // Abrir línea de datos de origen
+                    sourceDataLine.open(audioFormat);
+                    sourceDataLine.start();
 
-					// Cerrar línea de datos de origen
-					sourceDataLine.drain();
-					sourceDataLine.stop();
-					sourceDataLine.close();
-				}
+                    // Configurar el volumen de la línea de datos de origen
+                    FloatControl gainControl = (FloatControl) sourceDataLine.getControl(FloatControl.Type.MASTER_GAIN);
+                    float dB = (float) (Math.log(volume) / Math.log(10.0) * 20.0);
+                    gainControl.setValue(dB);
+
+                    // Reproducir audio
+                    byte[] buffer = new byte[bufferSize];
+                    while (true) {
+                        int bytesRead = oggIn.read(buffer, 0, bufferSize);
+                        if (bytesRead == -1) {
+                            break;
+                        }
+                        sourceDataLine.write(buffer, 0, bytesRead);
+                    }
+
+                    // Cerrar línea de datos de origen
+                    sourceDataLine.drain();
+                    sourceDataLine.stop();
+                    sourceDataLine.close();
+                }
             } catch (Exception e) {
                 e.printStackTrace();
             }
@@ -75,16 +61,10 @@ public class VoiceBox {
         audioThread.start();
     }
 
-    public void stopAudio() {
-        isPlaying = false;
-        audioThread = null;
+    private void setFilePath(String filePath) {
     }
 
-	public String getFilePath() {
-		return filePath;
-	}
-
-	public void setFilePath(String filePath) {
-		this.filePath = filePath;
-	}
+    public void setVolume(float volume) {
+        this.volume = volume;
+    }
 }
