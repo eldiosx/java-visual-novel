ce906f26b26c21cbb0e785ffb4fc1450668c287c
AidaFdz <aidafdz@hotmail.com>
2023-04-19 14:31:14 +0200

    Niña muerta ya funciona

---
+++ java/src/application/Ep1Murder/Ep1Murder.java
@@ -0,0 +1,32 @@
+package application.Ep1Murder;
+
+import java.util.*;
+import application.SoundBox;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.event.ActionEvent;
+import application.SoundBox;
+
+
+public class Ep1Murder extends Application{
+	//Point And Click Niña Muerta
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("Ep1NinaMuerta.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Episodio 1");
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+
+    }
+    
+
+}
---
+++ java/src/application/Ep1Murder/MurderMethods.java
@@ -0,0 +1,97 @@
+package application.Ep1Murder;
+
+import java.util.Scanner;
+
+public class MurderMethods {
+	
+	 Scanner read= new Scanner (System.in);
+     
+     /*
+      * Episodio 1. Imagen general con la hoguera y los 5 diálogos posibles
+      */
+     
+     int cont=0;
+     int personaje=0;
+     boolean john=false;
+     boolean travis=false;
+     boolean marleene=false;
+     boolean helen=false;
+     boolean chan=false;
+     
+     while (cont <=3) {
+     	
+     	personaje= read.nextInt();
+     	
+     	switch (personaje)  {
+
+			case 1:
+				System.out.println("Dialogo de John");
+			case 2:
+				System.out.println("Dialogo de Travis");
+			case 3:
+				System.out.println("Dialogo de Marleene");
+			case 4:
+				System.out.println("Diálogo de Helen");
+			case 5:
+				System.out.println("Dialogo de Chan");
+			default:
+				System.out.println("Elige un personaje");
+     	}
+
+     }
+}
+
+     public void john () {
+    /*
+     * Diálogo de John
+     * Llamada imagen de John
+     * Llamada al dialogo de John
+     * Llamada audio John
+     */
+    }
+    
+    
+    
+    public void travis () {
+    /*
+     * Diálogo de Travis
+     * Llamada imagen de Travis
+     * Llamada al dialogo de Travis
+     * Llamada audio Travis
+     */
+    }
+    
+    
+    
+    public void marleene () {
+    /*
+     * Diálogo de Marleene
+     * Llamada imagen de Marleene
+     * Llamada al dialogo de Marleene
+     * Llamada audio Marleene
+     */
+    	
+    }
+    
+    
+    
+    public void helen () {
+    /*
+     * Diálogo de Helen
+     * Llamada imagen de Helen
+     * Llamada al dialogo de Helen
+     * Llamada audio Helen
+     */
+    }
+    
+    
+    
+    public void chan () {
+    /*
+     * Diálogo de Chan
+     * Llamada imagen de Helen
+     * Llamada al dialogo de Helen
+     * Llamada audio Helen
+     */
+    	
+    }
