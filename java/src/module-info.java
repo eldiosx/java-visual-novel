8398d857f323f1e628ea5a131193fad9bd59cf78
Daniel Delgado Jiménez <danieldelgadojimenez@protonmail.com>
2023-04-20 15:55:13 +0200

    purge

--- java/src/module-info.java
+++ java/src/module-info.java
@@ -9,19 +9,10 @@ module java {
 	requires java.desktop; // AWT and Swing
 	requires mysql.connector.j; //MySQL
 	
-<<<<<<< HEAD
 	opens application to javafx.graphics, javafx.fxml, jdx; // Abrir el paquete a los modulos
 	opens application.Prologue to javafx.graphics, javafx.fxml, jdx; // Abrir el paquete a los modulos
 	opens application.Ep1 to javafx.graphics, javafx.fxml, jdx; // Abrir el paquete a los modulos
 	exports application.Prologue to javafx.graphics, javafx.fxml, jdx; // Exportar el paquete a los modulos
 	exports application.Ep1 to javafx.graphics, javafx.fxml, jdx; // Exportar el paquete a los modulos
-=======
-	opens application to javafx.graphics, javafx.fxml;
-	exports application.Ep1 to javafx.graphics, javafx.fxml;
-	exports application.Prologue to javafx.graphics, javafx.fxml;
-	exports application.Ep1Murder to javafx.graphics, javafx.fxml;
-	opens application.Ep1 to javafx.fxml; // Agregar esta línea para abrir el paquete al módulo javafx.fxml
-	opens application.Ep1Murder to javafx.fxml;
->>>>>>> 67d6df3004a0018376f16383817628d8028fc3fe
     exports application;
 }
