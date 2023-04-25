83abb0ea9c5bbb0a6eef8ca8652277e1f032b19f
Daniel Delgado Jiménez <danieldelgadojimenez@protonmail.com>
2023-04-25 13:59:09 +0200

    66546

--- java/src/application/ep1/Stage2.java
+++ java/src/application/ep1/Stage2.java
@@ -1,6 +1,7 @@
 package application.ep1;
 
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 
 public class Stage2 {
@@ -8,6 +9,7 @@ public class Stage2 {
 	@FXML
 	private Button myButton2;
 
+<<<<<<< Updated upstream
 	@FXML
 	public void initialize() {
 		myButton2.setOnAction(event -> {
@@ -23,3 +25,140 @@ public class Stage2 {
 		});
 	}
 }
+=======
+	    @FXML // fx:id="mano"
+	    private Button mano; // Value injected by FXMLLoader
+
+	    @FXML // fx:id="myButton2"
+	    private Button myButton2; // Value injected by FXMLLoader
+
+	    @FXML // fx:id="myButton21111"
+	    private Button myButton21111; // Value injected by FXMLLoader
+
+	    @FXML // fx:id="sala"
+	    private Button sala; // Value injected by FXMLLoader
+
+	    @FXML // fx:id="sangre"
+	    private Button sangre; // Value injected by FXMLLoader
+
+	    @FXML // fx:id="suelo"
+	    private Button suelo; // Value injected by FXMLLoader
+
+	    @FXML // fx:id="textocuchillo"
+	    private Label textocuchillo; // Value injected by FXMLLoader
+
+	    @FXML // fx:id="textocuerpo"
+	    private Label textocuerpo; // Value injected by FXMLLoader
+
+	    @FXML // fx:id="textomano"
+	    private Label textomano; // Value injected by FXMLLoader
+
+	    @FXML // fx:id="textosala"
+	    private Label textosala; // Value injected by FXMLLoader
+
+	    @FXML // fx:id="textosangre"
+	    private Label textosangre; // Value injected by FXMLLoader
+
+	    @FXML // fx:id="textosuelo"
+	    private Label textosuelo; // Value injected by FXMLLoader
+	    
+	    
+	    int count = 0;
+	    boolean isRunning = true;
+
+	    @FXML
+	    public void initialize() {
+
+			
+	        //Evento boton de sangre
+			suelo.setOnAction(event -> {
+	            try {
+	            	textosuelo.setVisible(true);
+	            } catch (Exception e) {
+	                e.printStackTrace();
+	            }
+	            count++; //incrementamos el contador
+	            if (count >= 3) { //si ya se pulsaron 4 botones
+	                disableRemainingButtons(); //desactivamos los botones restantes
+	            }
+	        });
+			
+	        //Evento boton de sangre
+			sala.setOnAction(event -> {
+	            try {
+	            	textosala.setVisible(true);
+	            } catch (Exception e) {
+	                e.printStackTrace();
+	            }
+	            count++; //incrementamos el contador
+	            if (count >= 3) { //si ya se pulsaron 4 botones
+	                disableRemainingButtons(); //desactivamos los botones restantes
+	            }
+	        });
+
+	        //Evento boton de sangre
+			mano.setOnAction(event -> {
+	            try {
+	            	textomano.setVisible(true);
+	            } catch (Exception e) {
+	                e.printStackTrace();
+	            }
+	            count++; //incrementamos el contador
+	            if (count >= 3) { //si ya se pulsaron 4 botones
+	                disableRemainingButtons(); //desactivamos los botones restantes
+	            }
+	        });
+			
+	        //Evento boton de sangre
+			cuchillo.setOnAction(event -> {
+	            try {
+	            	textocuchillo.setVisible(true);
+	            } catch (Exception e) {
+	                e.printStackTrace();
+	            }
+	            count++; //incrementamos el contador
+	            if (count >= 3) { //si ya se pulsaron 4 botones
+	                disableRemainingButtons(); //desactivamos los botones restantes
+	            }
+	        });
+
+	        //Evento boton de sangre
+	        sangre.setOnAction(event -> {
+	            try {
+	                textosangre.setVisible(true);
+	            } catch (Exception e) {
+	                e.printStackTrace();
+	            }
+	            count++; //incrementamos el contador
+	            if (count >= 3) { //si ya se pulsaron 4 botones
+	                disableRemainingButtons(); //desactivamos los botones restantes
+	            }
+	        });
+	        
+	        //Evento boton de sangre
+	        cuerpo.setOnAction(event -> {
+	            try {
+	            	textocuerpo.setVisible(true);
+	            } catch (Exception e) {
+	                e.printStackTrace();
+	            }
+	            count++; //incrementamos el contador
+	            if (count >= 3) { //si ya se pulsaron 4 botones
+	                disableRemainingButtons(); //desactivamos los botones restantes
+	            }
+	        });
+
+	    }
+
+	    private void disableRemainingButtons() {
+	        for (Node node : myButton2.getParent().getChildrenUnmodifiable()) {
+	            if (node instanceof Button && !node.isDisabled()) { //si es un botón habilitado
+	                ((Button) node).setDisable(true); //lo desactivamos
+	            }
+	        }
+	    }
+	}
+	
+
+
+>>>>>>> Stashed changes
