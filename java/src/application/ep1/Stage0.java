d95c2c661fbbcbf086488510f547dbda9119b3bf
AidaFdz <aidafdz@hotmail.com>
2023-04-25 13:57:25 +0200

    escena 0 ep 1

---
+++ java/src/application/ep1/Stage0.java
@@ -0,0 +1,113 @@
+package application.ep1;
+
+import java.io.File;
+
+import application.BackgroundMusic;
+import application.VoiceBox;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+public class Stage0 {
+	private static final String RESOURCES_PATH = new File("assets").getAbsolutePath();
+	private VoiceBox voiceBox = new VoiceBox();
+	private BackgroundMusic backgroundMusic = new BackgroundMusic();
+
+
+	@FXML
+	private Button John;
+	
+	@FXML
+	private Button Marleene;
+	
+	@FXML
+	private Button Helen;
+	
+	@FXML
+	private Button Chan;
+	
+	@FXML
+	private Button Travis;
+
+	@FXML
+	public void initialize() {
+		backgroundMusic.playAudio(RESOURCES_PATH + "/audio/firecamp.ogg");
+		//voiceBox.playAudio(RESOURCES_PATH + "/audio/jhon.ogg");
+		John.setOnAction(event -> {
+			try {
+				// llamada a la escena de John
+
+				// Mostrar el nuevo Stage, que es el de John
+				//Main.showStage(stage);
+
+				// Ocultar el Stage principal
+				Main.hideStage(Main.getPrimaryStage());
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		});
+		
+		Marleene.setOnAction(event -> {
+			try {
+				// llamada a la escena de Marleene
+
+				// Mostrar el nuevo Stage, que es el de Marleene
+				//Main.showStage(stage);
+
+				// Ocultar el Stage principal
+				Main.hideStage(Main.getPrimaryStage());
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		});
+		
+		Helen.setOnAction(event -> {
+			try {
+				// llamada a la escena de Helen
+
+				// Mostrar el nuevo Stage, que es el de Helen
+				//Main.showStage(stage);
+
+				// Ocultar el Stage principal
+				Main.hideStage(Main.getPrimaryStage());
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		});
+		
+		Chan.setOnAction(event -> {
+			try {
+				// llamada a la escena de Chan
+
+				// Mostrar el nuevo Stage, que es el de Chan
+				//Main.showStage(stage);
+
+				// Ocultar el Stage principal
+				Main.hideStage(Main.getPrimaryStage());
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		});
+		
+		Travis.setOnAction(event -> {
+			try {
+				// llamada a la escena de Travis
+
+				// Mostrar el nuevo Stage, que es el de Travis
+				//Main.showStage(stage);
+
+				// Ocultar el Stage principal
+				Main.hideStage(Main.getPrimaryStage());
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		});
+		;
+
+	}
+}
