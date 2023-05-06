7656c8e01a9bf005f264d9fc5ee16133d215a7e3
Daniel Delgado Jiménez <danieldelgadojimenez@protonmail.com>
2023-05-06 14:46:49 +0200

    endUpdate

--- java/src/application/end/Credits.java
+++ java/src/application/end/Credits.java
@@ -1,8 +1,9 @@
 package application.end;
-
+//Java
 import java.io.File;
-
-import application.BackgroundMusic;
+import java.util.Timer;
+import java.util.TimerTask;
+//JavaFX
 import javafx.animation.TranslateTransition;
 import javafx.application.Application;
 import javafx.application.Platform;
@@ -14,6 +15,9 @@ import javafx.scene.text.Text;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 import javafx.util.Duration;
+//Local
+import application.BackgroundMusic;
+
 
 public class Credits extends Application {
 
@@ -25,8 +29,23 @@ public class Credits extends Application {
 
 	@Override
 	public void start(Stage stage) {
+		// Audio de fondo
 		backgroundMusic.playAudio(RESOURCES_PATH + "/audio/horrorBellaciao.ogg");
-		// Crear el texto que se mostrará en los créditos
+
+		// Programar una tarea que se ejecute después de 30 segundos
+		Timer timer = new Timer();
+		timer.schedule(new TimerTask() {
+			@Override
+			public void run() {
+				Platform.runLater(() -> {
+					// Cerrar la aplicación
+					Platform.exit(); // El cierratodo de JavaFX
+					System.exit(0); // El cierratodo clasico
+				});
+			}
+		}, 40000); // 40 segundos en milisegundos
+
+		// Crear el texto que se mostrará en los créditos y su estilo
 		Text creditsText = new Text("Hace muchos tiempo, en un MEDAC casi en Córdoba, un equipo de desarrolladores \n\n"
 				+ "empezaron un ambicioso proyecto. \n\n " + "Los disturbios rodean el MEDAC \n\n"
 				+ "Se esta planteando el cobro de impuestos al temario desactualizado \n\n"
@@ -37,14 +56,16 @@ public class Credits extends Application {
 				+ "Desarrollado por Kawaii Developers \n\n"
 				+ "Nico Cano, Daniel Delgado, Aida Fernández y Adri Márquez\n\n" + "\n\n" + "\n\n"
 				+ "Proyecto realizado usando:\n\n" + "Java \n\n" + "Java FX \n\n" + "Scene Builder \n\n" + "SQL \n\n"
-				+ "Audacity \n\n" + "\n\n" + "\n\n" + "Desarrollo: \n\n" + "Database Manager: Nico Cano\n\n "
-				+ "Diseño: Aida Fernández\n\n" + "Frontend Manager: Adrián Marquez\n\n"
-				+ "Backend Manager: Daniel Delgado\n\n" + "\n\n" + "\n\n" + "Actores de voz: \n\n"
-				+ "John: Nico Cano \n\n" + "Marleene: Esther Argüelles\n\n" + "Travis: Javier Naranjo\n\n"
-				+ "Helen: Aida Fernández\n\n" + "Chan: Adri Marquez \n\n" + "Sr Watson: Daniel Delgado \n\n" + "\n\n"
-				+ "\n\n" + "Agradecimientos especiales \n\n"
+				+ "Audacity \n\n" + "Gimp \n\n" + "\n\n" + "\n\n" + "Desarrollo: \n\n"
+				+ "Database Manager: Nico Cano\n\n " + "Diseño: Aida Fernández\n\n"
+				+ "Frontend Manager: Adrián Marquez\n\n" + "Backend Manager: Daniel Delgado\n\n" + "\n\n" + "\n\n"
+				+ "Actores de voz: \n\n" + "John: Nico Cano \n\n" + "Marleene: Esther Argüelles\n\n"
+				+ "Travis: Javier Naranjo\n\n" + "Helen: Aida Fernández\n\n" + "Chan: Adri Marquez \n\n"
+				+ "Sr Watson: Daniel Delgado \n\n" + "\n\n" + "\n\n" + "Agradecimientos especiales \n\n"
 				+ "Javier naranjo y Esther Argüelles por trabajar a cambio de un vinito \n\n"
-				+ "Juan García y Antonio Ruiz por sus consejos y paciencia \n\n");
+				+ "Juan García y Antonio Ruiz por sus consejos y paciencia \n\n" + "\n\n" + "\n\n" + "\n\n"
+				+ "Repositorio de Github: \n\n" + "https://github.com/eldiosx/java-visual-novel \n\n \n\n"
+				+ "FIN <3 \n\n \n\n");
 		creditsText.setFont(new Font("Arial", responsive / 4));
 		creditsText.setFill(Color.WHITE);
 		creditsText.setX(500);
@@ -70,7 +91,7 @@ public class Credits extends Application {
 		// Mostrar la escena
 		stage.setScene(scene);
 		stage.setTitle("Tu videojuego favorito de serie B");
-		stage.setResizable(true); // Se puede redimensionar
+		stage.setResizable(false); // Se puede redimensionar
 		stage.setFullScreen(true); // Abre la ventana en pantalla completa
 		stage.setMinWidth(800); // Establece el ancho mínimo de la ventana en 800px
 		stage.setMinHeight(600); // Establece la altura mínima de la ventana en 600px
